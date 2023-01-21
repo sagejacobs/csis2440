e20391e27b19c57908dc2093c50a903dfba560a6
sagejacobs <tyler.q.jacobs@gmail.com>
2023-01-21 15:22:52 -0700

    initial commit

---
+++ birthday/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html lang="en">
+    <head>
+        <title>birthday page</title>
+    </head>
+    <body>
+    <?php
+    $balloon = "./img/balloon.webp";
+    echo "<h1>Happy Birthday!</h1>";
+    echo "<img src='$balloon' alt='balloons!'>"
+    ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    echo "<h1>This is Sage Jacobs page</h1>";
+    echo "<p>Someday, this will be an awesome page</p>";
